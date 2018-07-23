edbbf8e28f7d1637dc683d4651fff6e72d0767c4
olivial24 <olivia.lorenz24@gmail.com>
2018-07-23 19:37:20 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,11 +1,7 @@
-"hello".reverse
-"hello".upcase 
-"friends".swapcase 
-"friends".gsub("s" "z")
-
 def crazystrings
 "hello".reverse
 "hello".upcase 
 "friends".swapcase 
 "friends".gsub("s" "z")
-end
+end 
+
