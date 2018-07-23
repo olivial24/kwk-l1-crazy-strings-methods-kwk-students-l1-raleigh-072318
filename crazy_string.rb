ec0f17a607b4a377194a76118f69403d4a3edc06
olivial24 <olivia.lorenz24@gmail.com>
2018-07-23 19:41:23 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,4 +1,8 @@
-def crazystrings
+def crazy_strings(a,b)
+  a=a.reverse.upcase
+  b=b.gsub("S""Z").gsub("s""z").swapcase
+end
+puts crazy_strings("hello friends")
 "hello".reverse
 "hello".upcase 
 "friends".swapcase 
